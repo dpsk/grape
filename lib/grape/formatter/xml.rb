b6cf649a94084c7a54356f7cb71498f4754605d5
Mikhail Nikalyukin <idups1k@gmail.com>
2013-12-16 16:36:28 +0200

    backport xml_format to_s for v0.4.1

--- lib/grape/formatter/xml.rb
+++ lib/grape/formatter/xml.rb
@@ -5,6 +5,7 @@ module Grape
 
         def call(object, env)
           return object.to_xml if object.respond_to?(:to_xml)
+          return object.to_s   if object.respond_to?(:to_s)
           raise Grape::Exceptions::InvalidFormatter.new(object.class, 'xml')
         end
 
